582942e2a4f6b324972a417122c4ca3d662cd9e8
Jake Good <jake@dropio.com>
2008-11-06 13:33:05 -0600

    This should really be a configured value

--- app/models/asset_mailer.rb
+++ app/models/asset_mailer.rb
@@ -2,7 +2,7 @@ class AssetMailer < ActionMailer::Base
 
   def asset(asset, emails, message = nil)
     recipients emails
-    from "\"uSend.io\" <no-reply@usend.io>"
+    from "\"uSend.io\" <usendio@drop.io>"
     subject "You've been sent a file from usend.io"
     body :asset => asset, :message => message
     content_type "text/html"
